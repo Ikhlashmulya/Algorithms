71c813446dc5cd60d9544848ac163843dcbcc22c
Ikhlashmulya <94058040+Ikhlashmulya@users.noreply.github.com>
2022-04-22 05:02:27 +0700

    Add files via upload

---
+++ Sorting/selectionsort.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]){
+	
+	
+	int list[100];
+	int n;
+	
+	cout << "Masukan Jumlah Array: ";
+	cin >> n;
+	
+	for(int i = 0;i < n;i++){
+		cout << "Masukan Array Ke - " << i + 1 << " : ";
+		cin >> list[i];
+	}
+	
+	
+	cout << endl;
+	cout << "Isi Array Sebelum pengurutan" << endl;
+	for(int i = 0;i < n;i++){
+		cout << list[i] << " | ";
+	}
+	cout << endl << endl;
+	
+	
+	cout << "Proses Pengurutan" << endl;
+	int min, temp;
+	for(int i = 0;i < n;i++){
+		min = i;
+		
+		for(int j = i + 1;j < n;j++){
+			if(list[min] > list[j]){
+				min = j;
+			}
+		}
+		
+		temp = list[i];
+		list[i] = list[min];
+		list[min] = temp;
+		
+		for(int m = 0;m < n;m++){
+		cout << list[m] << " | ";
+		}
+		cout << endl;
+		
+	}
+	
+	
+	cout << endl;
+	cout << "Isi Array Sesudah pengurutan" << endl;
+	for(int i = 0;i < n;i++){
+		cout << list[i] << " | ";
+	}
+	cout << endl;
+	
+	return 0;
+}
